549f76636f6aa16a3f34540e17f009f73bf24e81
nemmtor <kacper0witas@gmail.com>
2020-08-04 23:45:40 +0200

    Updates

---
+++ precache-manifest.f14a628333afaa03278b718b3ef5d743.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808f0f91cc13058c0c5b9f925445a4b6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "80fb818216872ad62be8",
+    "url": "/portfolio/static/css/main.85f0896e.chunk.css"
+  },
+  {
+    "revision": "88fd3d38dacd714cdc02",
+    "url": "/portfolio/static/js/2.024bed6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.024bed6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80fb818216872ad62be8",
+    "url": "/portfolio/static/js/main.c6c38d5d.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "d9e88f0b27b4e76eb286a145c96f848c",
+    "url": "/portfolio/static/media/arrow-right.d9e88f0b.svg"
+  },
+  {
+    "revision": "529bf9cdff934f9ce5f709e3c2e9073d",
+    "url": "/portfolio/static/media/babel.529bf9cd.svg"
+  },
+  {
+    "revision": "c4ce29f3d8d818e21c893bde4d815089",
+    "url": "/portfolio/static/media/bootstrap.c4ce29f3.svg"
+  },
+  {
+    "revision": "bb026ce279d70001f63b2f700d521922",
+    "url": "/portfolio/static/media/cosmonaut.bb026ce2.png"
+  },
+  {
+    "revision": "fdcbdb597365f89558a8237da4740ac5",
+    "url": "/portfolio/static/media/gimp.fdcbdb59.svg"
+  },
+  {
+    "revision": "6f4b432cc4943ebab8ad595915222759",
+    "url": "/portfolio/static/media/git.6f4b432c.svg"
+  },
+  {
+    "revision": "3af582e56c213a6439e38c3f360bd633",
+    "url": "/portfolio/static/media/heroku.3af582e5.svg"
+  },
+  {
+    "revision": "7997c4244e7a8cf2b160803e1c741502",
+    "url": "/portfolio/static/media/html.7997c424.svg"
+  },
+  {
+    "revision": "0154328fc3e7bfcdb3d9ca42a6f848b1",
+    "url": "/portfolio/static/media/javascript.0154328f.svg"
+  },
+  {
+    "revision": "0b90ee8301b77c1a3e1348cb06d9f7e6",
+    "url": "/portfolio/static/media/logo.0b90ee83.svg"
+  },
+  {
+    "revision": "e83e60b310396562d703c9ccff616b91",
+    "url": "/portfolio/static/media/npm.e83e60b3.svg"
+  },
+  {
+    "revision": "12c15f06434abcc66615a8920203e45f",
+    "url": "/portfolio/static/media/php.12c15f06.svg"
+  },
+  {
+    "revision": "7cb3af45500abbb8120e29f6095d67f0",
+    "url": "/portfolio/static/media/presta.7cb3af45.png"
+  },
+  {
+    "revision": "e9f3d01c242e086f895bde131803b451",
+    "url": "/portfolio/static/media/react.e9f3d01c.svg"
+  },
+  {
+    "revision": "88c1047792044b547d1d3e066d3e18c4",
+    "url": "/portfolio/static/media/sass.88c10477.svg"
+  },
+  {
+    "revision": "3c2970caad593f3a5364e13e1f75f29b",
+    "url": "/portfolio/static/media/trello.3c2970ca.svg"
+  },
+  {
+    "revision": "0ecd0125f31e0bfc39a9d3e130ca6cd6",
+    "url": "/portfolio/static/media/typescript.0ecd0125.svg"
+  },
+  {
+    "revision": "883863a6657dca9e5201132dc3a725b1",
+    "url": "/portfolio/static/media/webpack.883863a6.svg"
+  }
+]);
