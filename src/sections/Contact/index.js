b04949e4c0bfa04b01172f84a3f53d5b3561c9e2
nemmtor <kacper0witas@gmail.com>
2020-08-16 22:53:00 +0200

    Adds lots of nice styling changes

--- src/sections/Contact/index.js
+++ src/sections/Contact/index.js
@@ -8,37 +8,36 @@ const Contact = () => {
     console.log(e.target.name.value);
   };
   return (
-    <div className="contact__container">
-      <form
-        autoComplete="off"
-        className="contact__form"
-        onSubmit={handleSubmit}
-      >
-        <label className="contact__label" htmlFor="name">
+    <div className="form__container">
+      <form autoComplete="off" className="form" onSubmit={handleSubmit}>
+        <label className="form__label" htmlFor="name">
           Name
-          <input className="contact__input" id="name" name="name" type="text" />
+          <input className="form__input" id="name" name="name" type="text" />
         </label>
-        <label className="contact__label" htmlFor="mail">
+        <label className="form__label" htmlFor="mail">
           E-mail
-          <input className="contact__input" id="mail" name="mail" type="text" />
+          <input className="form__input" id="mail" name="mail" type="text" />
         </label>
-        <label className="contact__label" htmlFor="mail">
+        <label className="form__label" htmlFor="mail">
           Message
-          <textarea className="contact__input contact__input--msg" name="msg" />
+          <textarea className="form__input form__input--msg" name="msg" />
         </label>
-        <input className="contact__submit" type="submit" value="Send" />
+        <input className="form__submit" type="submit" value="Send" />
       </form>
-      <p className="contact__text">
+      <p className="form__text">
         <strong>Did you know?</strong>
-        <span className="contact__text-part">
+        <span className="form__text-part">
           90% of the web users never use contact form even if they are
           intesrested in getting in touch. Let&apos;s change that - send me your
           job offer details via this form!
         </span>
-        <span className="contact__text-part">
+        <span className="form__text-part">
           Feel free to leave me a message if you&apos;re interested in hiring
           me, want to collaborate or just have a tech or not tech related chat.
           I&apos;ll reply as soon as possible.
+        </span>
+        <span className="form__text-part">
+          As an alternative you can find me here:
         </span>
       </p>
     </div>
