2e1bdf1ac383e9f6885e92c27921b8ebd2ed9d56
nemmtor <kacper0witas@gmail.com>
2020-08-21 09:45:58 +0200

    Refactor skills and projects

--- src/sections/Projects/Project.js
+++ src/sections/Projects/Project.js
@@ -1,76 +1,18 @@
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/all';
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-import { upArrow, world } from 'assets';
+import { world } from 'assets';
 import { github } from 'assets/tech-icons';
 
 const Project = ({ details }) => {
   const { description, img, stack, title, urls } = details;
 
-  const [isExpanded, setIsExpanded] = useState(false);
-
-  const toggleProjectDetails = () => {
-    setIsExpanded(!isExpanded);
-  };
-
-  const projectToggleRef = useRef(null);
-
-  useEffect(() => {
-    const timeline = gsap
-      .timeline({
-        defaults: { duration: 0.5, ease: 'power4.out' },
-      })
-      .fromTo(
-        projectToggleRef.current,
-        {
-          opacity: 0,
-          y: -50,
-          scale: 2,
-          duration: 1,
-          transformOrigin: 'center',
-          background: '#a34bed',
-        },
-        {
-          opacity: 1,
-          y: -50,
-          scale: 2,
-        },
-      )
-      .to(projectToggleRef.current, { y: 0, scale: 1 }, '-=0.25')
-      .to(projectToggleRef.current, { background: '#0e0e0e' }, '-=0.25');
-
-    ScrollTrigger.create({
-      trigger: projectToggleRef.current,
-      animation: timeline,
-      start: '-=250% center',
-    });
-  }, []);
-
   return (
     <article className="project">
-      <div
-        className={`project__image-container ${
-          isExpanded ? 'project__image-container--blurred' : ''
-        }`}
-      >
+      <div className="project__image-container">
         <img alt={img.alt} className="project__image" src={img.src} />
       </div>
-      <div
-        className={`project__details ${
-          isExpanded ? 'project__details--active' : ''
-        }`}
-      >
-        <button
-          className="project__toggle"
-          onClick={toggleProjectDetails}
-          ref={projectToggleRef}
-          type="button"
-        >
-          <img alt="Arrow up" src={upArrow} />
-        </button>
-
+      <div className="project__details">
         <h3 className="project__title">{title}</h3>
         <p className="project__description">{description}</p>
         <div className="project__stack">
@@ -84,19 +26,11 @@ const Project = ({ details }) => {
             />
           ))}
         </div>
-        <a
-          className="project__link"
-          href={urls.repo}
-          tabIndex={isExpanded ? '0' : '-1'}
-        >
+        <a className="project__link" href={urls.repo}>
           <img alt="Github" className="project__link-icon" src={github} />
           Code
         </a>
-        <a
-          className="project__link"
-          href={urls.live}
-          tabIndex={isExpanded ? '0' : '-1'}
-        >
+        <a className="project__link" href={urls.live}>
           <img alt="Github" className="project__link-icon" src={world} />
           Live
         </a>
