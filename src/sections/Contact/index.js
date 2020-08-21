5c28d990abb9878f709c74a8d4a70a4a81a2cd0b
nemmtor <kacper0witas@gmail.com>
2020-08-21 15:15:33 +0200

    Add form submit

--- src/sections/Contact/index.js
+++ src/sections/Contact/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './styles.scss';
 
 const Contact = () => {
-  const handleSubmit = (e) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isFinished, setIsFinished] = useState(false);
+
+  const handleSubmit = async (e) => {
+    setIsLoading(true);
     e.preventDefault();
-    console.log(e.target.name.value);
+    const form = e.target;
+    const body = {
+      name: form.name.value,
+      _replyto: form.mail.value,
+      message: form.message.value,
+    };
+    const res = await fetch('https://formspree.io/xzbjqazr', {
+      method: 'POST',
+      body: JSON.stringify(body),
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const resJson = await res.json();
+    setIsFinished(true);
+    setIsLoading(false);
   };
+
   return (
     <div className="form__container">
       <form autoComplete="off" className="form" onSubmit={handleSubmit}>
@@ -16,13 +37,19 @@ const Contact = () => {
         </label>
         <label className="form__label" htmlFor="mail">
           E-mail
-          <input className="form__input" id="mail" name="mail" type="text" />
+          <input className="form__input" id="mail" name="mail" type="email" />
         </label>
         <label className="form__label" htmlFor="mail">
           Message
-          <textarea className="form__input form__input--msg" name="msg" />
+          <textarea className="form__input form__input--msg" name="message" />
         </label>
-        <input className="form__submit" type="submit" value="Send" />
+        <input
+          className="form__submit"
+          disabled={isLoading}
+          type="submit"
+          value="Send"
+        />
+        {isFinished && <span className="form__greetings">Thank you!</span>}
       </form>
       <p className="form__text">
         <strong>Did you know?</strong>
@@ -35,9 +62,6 @@ const Contact = () => {
           Feel free to leave me a message if you&apos;re interested in hiring
           me, want to collaborate or just have a tech or not tech related chat.
           I&apos;ll reply as soon as possible.
-        </span>
-        <span className="form__text-part">
-          As an alternative you can find me here:
         </span>
       </p>
     </div>
